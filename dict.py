f71d42c35a71f6563e0d5a6fac0a4b6096e2f960
marihanson <hmarianne1@yahoo.com>
2022-06-09 16:01:40 +0200

    Fixing changes

--- dict.py
+++ dict.py
@@ -10,40 +10,44 @@ print('''   Welcome to the dictionary! Command options:
         add - add new word to the dictionary
         delete - delete word from dictionary
         quit - quit the program  ''')
+#create function to list all the entries
 def read_dict(conn):
     cur = conn.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
+#create function to add new entries
 def add_word(conn, word, translation):
     cur = conn.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
+#create a function to delete entries by word ID
 def delete_word(conn, ID):
     cur = conn.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
+#create a function to save changes made
 def save_dict(conn):
     cur = conn.cursor()
     cur.execute("COMMIT;")
     cur.close()
 def main():
- while True: ## REPL - Read Execute Program Loop
-    cmd = input("Command: ")
-    if cmd == "list":
-        for i, wd, trans in read_dict(conn):
-            print(f"{i}: {wd} - {trans}")
-    elif cmd == "add":
-        word = input("  Word: ")
-        translation = input("  Translation: ")
-        add_word(conn, word, translation)
-        print(f" Added word {word}")
-    elif cmd == "delete":
-        ID = input("  ID: ")
-        delete_word(conn, ID)
-        print(f" Deleted word {word}")
-    elif cmd == "quit":
-        save_dict(conn)
-        exit()
+    while True: ## REPL - Read Execute Program Loop
+        cmd = input("Command: ")
+        if cmd == "list":
+            for i, wd, trans in read_dict(conn):
+                print(f"{i}: {wd} - {trans}")
+        elif cmd == "add":
+            word = input("  Word: ")
+            translation = input("  Translation: ")
+            add_word(conn, word, translation)
+            print(f" Added word {word}")
+        elif cmd == "delete":
+            ID = input("  ID: ")
+            delete_word(conn, ID)
+            print(f" Deleted word {word}")
+        elif cmd == "quit":
+            save_dict(conn)
+            exit()
 main()
